ecd35b0cc6998dbd222ae1544900ff3f8e209834
Talesrib <talespiano@gmail.com>
2024-09-19 15:53:17 -0300

    add register to register no fileshare server

--- fileshare_server/main.go
+++ fileshare_server/main.go
@@ -6,15 +6,18 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	pb "filesharep2p/fileshare"
+	reg "filesharep2p/register"
 	sum "filesharep2p/sum"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port = flag.Int("port", 50052, "The server port")
 	sums = make(map[int][]string)
 )
 
@@ -30,15 +33,35 @@ func (s *server) HasFile(ctx context.Context, in *pb.MessageRequest) (*pb.Messag
 	return &pb.MessageResponse{HasFile: false}, nil
 }
 
-func main() {
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+
+	for _, addr := range addrs {
+		// Verifica se é do tipo IP
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			// Pega apenas endereços IPv4
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String()
+			}
+		}
+	}
+
+	return ""
+}
+
+func calculateSum() {
 	c := make(chan sum.Sums)
 	nt := sum.ReadFiles(c)
 	for i := 0; i < nt; i++ {
 		x := <-c
 		sums[x.Sum] = append(sums[x.Sum], x.Path)
 	}
+}
 
-	flag.Parse()
+func initiateServer() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -49,4 +72,28 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func register() {
+	ip := getLocalIP()
+	connReg, err := grpc.NewClient(ip+":50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Falha ao conectar ao servidor: %v", err)
+	}
+	defer connReg.Close()
+	client := reg.NewRegisterServiceClient(connReg)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	registerResp, err := client.Register(ctx, &reg.RegisterRequest{Address: fmt.Sprintf("%s:%d", ip, *port)})
+	if err != nil {
+		log.Fatalf("Erro ao registrar o peer: %v", err)
+	}
+	fmt.Print(registerResp.Message)
+}
+
+func main() {
+	flag.Parse()
+	go calculateSum()
+	go register()
+	initiateServer()
 }
