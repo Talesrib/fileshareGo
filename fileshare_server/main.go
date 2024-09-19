b38b3331ca43e6177a6e640959a58867341e0279
Talesrib <talespiano@gmail.com>
2024-09-19 13:14:25 -0300

    fileshare_server guarda estado dos arquivos

--- fileshare_server/main.go
+++ fileshare_server/main.go
@@ -15,6 +15,7 @@ import (
 
 var (
 	port = flag.Int("port", 50051, "The server port")
+	sums = make(map[int][]string)
 )
 
 type server struct {
@@ -23,18 +24,20 @@ type server struct {
 
 func (s *server) HasFile(ctx context.Context, in *pb.MessageRequest) (*pb.MessageResponse, error) {
 	log.Printf("Received: %v", in.GetHash())
-	c := make(chan sum.Sums)
-	nt := sum.ReadFiles(c)
-	for i := 0; i < nt; i++ {
-		x := <-c
-		if x.Sum == int(in.GetHash()) {
-			return &pb.MessageResponse{HasFile: true}, nil
-		}
+	if sums[int(in.GetHash())] != nil {
+		return &pb.MessageResponse{HasFile: true}, nil
 	}
 	return &pb.MessageResponse{HasFile: false}, nil
 }
 
 func main() {
+	c := make(chan sum.Sums)
+	nt := sum.ReadFiles(c)
+	for i := 0; i < nt; i++ {
+		x := <-c
+		sums[x.Sum] = append(sums[x.Sum], x.Path)
+	}
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
